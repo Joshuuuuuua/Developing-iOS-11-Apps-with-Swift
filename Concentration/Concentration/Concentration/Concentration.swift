4525f7f58db2ed700bd1f47214aa877648f7cf28
Joshuuuuuua <m775644356@hotmail.com>
2018-03-07 20:33:06 +0800

    NSAttributedString , String , Protocol , Hashable

--- Concentration/Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration/Concentration.swift
@@ -38,7 +38,7 @@ class Concentration {
         assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index) : chosen index not in the cards.")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                if cards[matchIndex].identifier == cards[index].identifier {
+                if cards[matchIndex] == cards[index] {
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
--- Concentration/Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/Concentration/ViewController.swift
@@ -18,11 +18,24 @@ class ViewController: UIViewController {
     
     private var flipCounts: Int = 0 {
         didSet {
-            flipCountLabel.text = "Flips : \(flipCounts)"
+            updateFlipCountLabel()
         }
     }
     
-    @IBOutlet private var flipCountLabel: UILabel!
+    private func updateFlipCountLabel() {
+        let attributes: [NSAttributedStringKey:Any] = [
+            .strokeWidth : 2.0,
+            .strokeColor : #colorLiteral(red: 1, green: 0.598092258, blue: 0.3952965736, alpha: 1)
+        ]
+        let attributedString = NSAttributedString(string: "Flips : \(flipCounts)", attributes: attributes)
+        flipCountLabel.attributedText = attributedString
+    }
+    
+    @IBOutlet private var flipCountLabel: UILabel! {
+        didSet {
+            updateFlipCountLabel()
+        }
+    }
     
     @IBOutlet private var cardButtons: [UIButton]!
     
@@ -56,15 +69,18 @@ class ViewController: UIViewController {
         }
     }
     
-    private var emojiChoices = ["👻","🎃","👾","🤖","💩","💀","🙀","👽"]
+    // private var emojiChoices = ["👻","🎃","👾","🤖","💩","💀","🙀","👽"]
+    private var emojiChoices = "👻🎃👾🤖💩💀🙀👽"
     
-    private var emoji = [Int: String]()
+    private var emoji = [Card: String]()
     
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            // emoji[card] = String(emojiChoices.remove(at: emojiChoices.count.arc4random))
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
     
 }
