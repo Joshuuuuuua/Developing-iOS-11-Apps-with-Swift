169bd5540e6901cf1073f2d26e291eb63ed9b470
Joshuuuuuua <m775644356@hotmail.com>
2018-03-15 12:29:25 +0800

    drag and drop

---
+++ Demos/EmojiArt/EmojiArt/EmojiCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt
+//
+//  Created by Joshua on 2018/3/15.
+//  Copyright © 2018年 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
